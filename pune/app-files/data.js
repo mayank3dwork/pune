23987b4ef580bae6c7ca54af94bfaa997c666e55
mayank3dwork <145151809+mayank3dwork@users.noreply.github.com>
2024-12-25 23:28:53 +0530

    Add files via upload

---
+++ pune/app-files/data.js
@@ -0,0 +1,113 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cam-01",
+      "name": "CAM 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7540440345673112,
+          "pitch": 0.15005634372273313,
+          "rotation": 0,
+          "target": "1-cam-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cam-02",
+      "name": "CAM 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8992770593003527,
+          "pitch": 0.3887036356322433,
+          "rotation": 0,
+          "target": "2-cam-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cam-03",
+      "name": "CAM 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9117629654153383,
+          "pitch": 0.16390371410905757,
+          "rotation": 0,
+          "target": "2-cam-03"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
